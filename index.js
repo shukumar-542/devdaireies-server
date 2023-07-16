bbf42584fe277e7a9afe55029fd4f2ca42dd4334
shukumar-542 <shukumar542@gmail.com>
2023-07-16 19:38:58 +0600

    new database

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://devDairies:PIsMBeZz4MGo7VSG@cluster0.66lxfzt.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://devDairies:4jj4EhEGu9h8Ri9S@cluster0.6jlixm1.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://devDairies:smS5I7TcQcdo4uPC@cluster0.6jlixm1.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,48 +31,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    const BlogCollection =  client.db('devdairies').collection('blogs');
+    const BlogCollection = client.db('devdairires').collection('blogs');
 
-    // get all blogs
     app.get('/blogs', async(req,res)=>{
-        const result = await BlogCollection.find().toArray();
-        res.send(result)
-    });
+      const result = await BlogCollection.find().toArray();
+      res.send(result)
+  });
 
-    // get blogs by category GO
-    app.get('/blogs/go', async(req,res)=>{
-        const result = await BlogCollection.find({category : 'Go'}).toArray()
-        res.send(result)
-    })
+  // get blogs by category GO
+  app.get('/blogs/go', async(req,res)=>{
+    const result = await BlogCollection.find({category : 'Go'}).toArray()
+    res.send(result)
+})
 
-    // get blogs by category javascript
-    app.get('/blogs/javascript', async(req,res)=>{
-        const result = await BlogCollection.find({category : 'javascript'}).toArray()
-        res.send(result)
-    })
+// get blogs by category javascript
+app.get('/blogs/javascript', async(req,res)=>{
+    const result = await BlogCollection.find({category : 'javascript'}).toArray()
+    res.send(result)
+})
 
-    // get blogs by category php
-    app.get('/blogs/php', async(req,res)=>{
-        const result = await BlogCollection.find({category : 'PHP'}).toArray()
-        res.send(result)
-    })
+// get blogs by category php
+app.get('/blogs/php', async(req,res)=>{
+    const result = await BlogCollection.find({category : 'PHP'}).toArray()
+    res.send(result)
+})
 
-    // get blogs by category Python
-    app.get('/blogs/python', async(req,res)=>{
-        const result = await BlogCollection.find({category : 'Python'}).toArray()
-        res.send(result)
-    })
-
-
-    // get blogs by ascending order by like
-    app.get('/blogs/populars', async(req,res)=>{
-        const result = await BlogCollection.find().sort({likes : -1}).limit(6).toArray();
-        res.send(result)
-    })
+// get blogs by category Python
+app.get('/blogs/python', async(req,res)=>{
+    const result = await BlogCollection.find({category : 'Python'}).toArray()
+    res.send(result)
+})
 
 
-
-
+// get blogs by ascending order by like
+app.get('/blogs/populars', async(req,res)=>{
+    const result = await BlogCollection.find().sort({likes : -1}).limit(6).toArray();
+    res.send(result)
+})
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -86,6 +80,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
 
 
