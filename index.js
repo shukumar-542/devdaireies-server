22688cb51f3b4bcbcb85cceded742604d425966d
shukumar-542 <shukumar542@gmail.com>
2023-07-23 23:21:43 +0600

    set user role

--- index.js
+++ index.js
@@ -196,6 +196,18 @@ async function run() {
       res.send(result)
     })
 
+      // set user role
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const updateDoc = {
+          $set: { role: 'admin' }
+        }
+        const result = await userCollection.updateOne(query, updateDoc);
+        res.send(result)
+      })
+  
+
 
 
 
